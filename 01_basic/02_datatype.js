fe0ae374ada8fa3c077f4c04ed200226ec823bba
SOHAN AMAHED SAMI <sohan.explorer@gmail.com>
2026-01-25 15:03:06 +0000

    datatype

---
+++ 01_basic/02_datatype.js
@@ -0,0 +1,25 @@
+// "use strict"; //treat all js code as newer version
+
+// alert("hello") // we are using nodejs. not brower
+
+// console.log()
+
+
+// let name = "sohan"
+//  let age = 20
+//  let isLoggedIn = false
+
+
+ // number  => 2 to power 53 
+ // bigint 
+ // string =>""
+ // boolean =>true/false
+ // null =>standalone value
+ // undefined =>
+ // symbol =>unique
+
+
+
+ //object 
+
+ console.log(typeof "sohan")
