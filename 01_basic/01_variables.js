4d05745339b9a86077e07f12523df6d380213b00
SOHAN AMAHED SAMI <sohan.explorer@gmail.com>
2026-01-25 14:32:21 +0000

    Add.variables

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 25625
+let accountEmail = "sohan@.com"
+var accountPassword = "12345"
+accountCity = "Dhaka"
+let accountState
+
+accountEmail = "sami@,com"
+
+accountPassword = "23434"
+accountCity = "gazipure"
+
+console.log(accountId);
+console.table([accountEmail,accountId,accountCity,accountState])
+
+
+/**
+ * Prefer not to use var because of issue in block scope and functional scope 
+ */
