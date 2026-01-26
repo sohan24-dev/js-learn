c9ec765504e33001f80979859659a83c721853af
sohan24-dev <sohan.explorer@gmail.com>
2026-01-26 18:15:25 +0600

    comparison

--- 01_basic/03_conbartion.js
+++ 01_basic/03_conbartion.js
@@ -44,4 +44,33 @@ let str3 = str1 + str2
 
 // console.log("1" + 2 + 2)
 // console.log(1 + 2 + "2")
-// console.log(2 + 3 + "5")
+// console.log(2 + 3 + "5" + 3)
+
+// let gameCountar = 100;
+// gameCountar++;
+// console.log(gameCountar);
+// let numberCountar = 100;
+// --numberCountar;
+// console.log(numberCountar);
+
+// Postfix increment ...........
+
+// let x = 3;
+// const y = x++;
+// console.log(x);
+// console.log(y);
+// x is 4; y is 3
+
+
+// let x2 = 3n;
+// const y2 = x2++;
+// x2 is 4n; y2 is 3n
+
+// Prefix increment ............
+
+// let p = 3;
+// const s = ++p;
+// console.log(p);
+// console.log(s);
+
+
