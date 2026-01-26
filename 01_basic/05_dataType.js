ad7b70ef6e13793526f7bd86656b96c0a05aaebe
sohan24-dev <sohan.explorer@gmail.com>
2026-01-26 18:42:11 +0600

    add dataTypes

---
+++ 01_basic/05_dataType.js
@@ -0,0 +1,33 @@
+// primitive 
+
+// 7 types : string ,Number ,Boolearn, null, undefined, symbol,BigInt
+
+const score = 100
+const scoreValue = 100
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+
+const bigNumber = 2343525453435534n
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["sohan", "sami", "sadia"];
+let myObj = {
+    name:"sohan",
+    age: 20,
+    }
+
+const myfunction = function() {
+    console.log("Hello world");
+    
+}
+console.log(typeof bigNumber);
