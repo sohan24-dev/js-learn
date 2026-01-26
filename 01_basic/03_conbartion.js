b6817672b846d6bed883786f86e2544bcb07b636
sohan24-dev <sohan.explorer@gmail.com>
2026-01-26 17:03:36 +0600

    add

--- 01_basic/03_conbartion.js
+++ 01_basic/03_conbartion.js
@@ -24,5 +24,24 @@
 let someNumber = 33;
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+/** Operations  ............. */
+let value = 3 ;
+let negValue = -value;
+// console.log(negValue)
+
+
+// console.log(2*3)
+
+let str1 = "hello"
+let str2 = " sohan"
+
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+// console.log(2 + 3 + "5")
