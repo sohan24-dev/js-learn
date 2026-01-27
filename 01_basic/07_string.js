e464cd3614af5a684952195d0b5184a087c440c2
sohan24-dev <sohan.explorer@gmail.com>
2026-01-27 18:21:23 +0600

    add string methrod

---
+++ 01_basic/07_string.js
@@ -0,0 +1,23 @@
+
+const name = "sohan";
+const repoCount = 50;
+
+// console.log(name + repoCount + "value");
+
+// `` its name of backtick
+// console.log(` Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String ("sohansami") //type object 
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+
+// const anotherString = gameName.slice(-8,4);
+// console.log(anotherString)
+
+
+
