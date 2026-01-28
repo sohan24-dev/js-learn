ee98fbe0b080bee5060a5d74c1fd4d6a56707f5a
sohan24-dev <sohan.explorer@gmail.com>
2026-01-28 16:06:52 +0600

    add num and math

---
+++ 01_basic/08.nums.math.js
@@ -0,0 +1,43 @@
+const score = 400;
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 23.25553;
+
+// console.log(otherNumber.toPrecision(3))
+
+const hundreds = 100000000;
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+
+//****************      Maths **************/
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.3));
+// console.log(Math.ceil(4.3));
+// console.log(Math.floor(4.3));
+// console.log(Math.max(3,53,5,6,3));
+
+
+
+// console.log(Math.random());
+// console.log(Math.round(Math.random()*10) + 1);
+
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
+
+
+
+
+
+
+
