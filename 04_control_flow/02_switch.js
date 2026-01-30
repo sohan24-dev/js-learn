fcaa656923527df5fcb08e3666a006a56d594a48
sohan24-dev <sohan.explorer@gmail.com>
2026-01-30 18:21:35 +0600

    switch

---
+++ 04_control_flow/02_switch.js
@@ -0,0 +1,24 @@
+
+
+const month = 3
+
+switch (month) {
+    case 1:
+        console.log("January");        
+        break;
+    case 2:
+        console.log("feb");        
+        break;
+    case 3:
+        console.log("March");        
+        break;
+    case 4:
+        console.log("april");        
+        break;
+
+    default:
+        console.log("Default case match");
+        
+        break;
+}
+
