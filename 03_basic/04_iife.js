bad48b681a3f781f4c25f9f0dff1213220e3e662
sohan24-dev <sohan.explorer@gmail.com>
2026-01-30 17:33:25 +0600

    iife

---
+++ 03_basic/04_iife.js
@@ -0,0 +1,13 @@
+// Immediately invoked funtion expressions (IIFE)
+
+
+(function chai (){
+    console.log(`DB CONNECTED`);
+    
+})();
+
+
+((age) => {
+    console.log(`Name sohan ${age}`);
+
+} )(20);
